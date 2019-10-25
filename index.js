f3ef9134668e53b6b736683e1c9914ad09fdcf62
Kaemon Lovendahl <k-manskrazy@hotmail.com>
2019-10-24 18:12:22 -0600

    Added a few more rules/plugins

--- index.js
+++ index.js
@@ -2,49 +2,15 @@ module.exports = {
   "extends": ["airbnb"],
   "plugins": [
     "prettier",
-    "jsx-a11y"
+    "jsx-a11y",
+    "import",
+    "react",
+    "react-hooks"
   ],
   "rules": {
     "prettier/prettier": "error",
-    "jsx-a11y/control-has-associated-label": [ 2, {
-      "labelAttributes": ["label"],
-      "controlComponents": ["CustomComponent"],
-      "ignoreElements": [
-        "audio",
-        "canvas",
-        "embed",
-        "input",
-        "textarea",
-        "tr",
-        "video",
-      ],
-      "ignoreRoles": [
-        "grid",
-        "listbox",
-        "menu",
-        "menubar",
-        "radiogroup",
-        "row",
-        "tablist",
-        "toolbar",
-        "tree",
-        "treegrid",
-      ],
-      "depth": 3,
-    }],
-    "react/jsx-fragments": [2, "syntax"],
-    "react/jsx-curly-newline": [2, {
-      multiline: "consistent",
-      singleline: "consistent" 
-    }],
-    "react/state-in-constructor": [2, "never"],
-    "react/static-property-placement": [2],
-    "react/jsx-props-no-spreading": [2, {
-      "html": "ignore",
-      "custom": "ignore",
-      "explicitSpread": "ignore",
-  }],
-  "linebreak-style": [2, "windows"],
-  "semi": [2, "never"]
+    "semi": [2, "never"],
+    "linebreak-style": 0,
+    "import/prefer-default-export": 0,
   }
 }
