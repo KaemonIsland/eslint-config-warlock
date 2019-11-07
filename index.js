9626eb86556fad78a1655f0a17c6103e5fdd655f
Kaemon Lovendahl <k-manskrazy@hotmail.com>
2019-11-06 19:16:25 -0700

    Add new rules, globals, and parserOptions

--- index.js
+++ index.js
@@ -8,9 +8,28 @@ module.exports = {
     "react-hooks"
   ],
   "rules": {
-    "prettier/prettier": "error",
     "semi": [2, "never"],
     "linebreak-style": 0,
     "import/prefer-default-export": 0,
-  }
+    "react/require-default-props": 0,
+    "arrow-parens": [2, "as-needed"],
+    "react/jsx-one-expression-per-line": 0,
+    "no-confusing-arrow": 0,
+    "react/jsx-props-no-spreading": 0
+  },
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+        "jsx": true
+      },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
 }
